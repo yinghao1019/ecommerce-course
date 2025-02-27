e121b3fcdd624099a86abf926cd2c459219d6e7d
Howard <z112517z@gmail.com>
2025-02-28 00:29:13 +0800

    feat: security config
    調整項目:
    1.新增security config邏輯

--- src/main/java/com/howhow/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/howhow/ecommerce/config/SecurityConfig.java
@@ -2,11 +2,30 @@ package com.howhow.ecommerce.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(csrf -> csrf.disable())
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults())
+                .authorizeHttpRequests(
+                        request ->
+                                request.requestMatchers("/members/signup")
+                                        .permitAll()
+                                        .anyRequest()
+                                        .authenticated())
+                .build();
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
